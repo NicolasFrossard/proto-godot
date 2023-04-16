b6059db2ad4bcdf343d957750a23daeed1db9394
nico <frossard.nicolas@gmail.com>
2023-04-16 19:10:31 +0200

    Add left choice support

--- dialogues/Dialogue.gd
+++ dialogues/Dialogue.gd
@@ -2,9 +2,13 @@ extends CanvasLayer
 
 export(String, FILE, "*.json") var dialogue_file
 
+onready var _anim_player := $MarginContainer/AnimationPlayer
+
 var dialogue = null
-var current_dialogue_id = 0
-	
+var next_script_index = 0
+
+var current_script_entry = null
+
 func play():
 	$ChoiceContainer.visible = false
 	dialogue = load_dialogue()
@@ -19,28 +23,37 @@ func load_dialogue():
 		return parse_json(file.get_as_text())
 
 func _on_Button_pressed():
+	if ('next_action' in current_script_entry && current_script_entry.next_action == 'QUIT_GAME'):
+		get_tree().quit()
+
 	next_line()
 
-func next_line():
-	current_dialogue_id += 1
-	
-	if (current_dialogue_id >= len(dialogue.entries)):
+func next_line():	
+	var entries_count = len(dialogue.entries)
+	if (next_script_index >= entries_count):
 		return
 		
 	read_line()
 
 func read_line():
-	var dialogue_entry = dialogue.entries[current_dialogue_id]
+	current_script_entry = dialogue.entries[next_script_index]
 	
-	if (dialogue_entry.mode == 'narration'):
-		read_narration_line(dialogue_entry)
+	if ('next_label' in current_script_entry):
+		$MarginContainer/ColorRect/NextButton.text = current_script_entry.next_label
+	else:
+		$MarginContainer/ColorRect/NextButton.text = '> Next'
 	
-	if (dialogue_entry.mode == 'conversation'):
-		read_conversation_line(dialogue_entry)
+	next_script_index += 1 # May be changed in case of choices
+	
+	if (current_script_entry.mode == 'narration'):
+		read_narration_line()
+	
+	if (current_script_entry.mode == 'conversation'):
+		read_conversation_line()
 		
-	if (dialogue_entry.mode == 'choice'):
+	if (current_script_entry.mode == 'choice'):
 		toggle_choice_mode(true)
-		read_choice_line(dialogue_entry)
+		read_choice_line()
 
 func toggle_choice_mode(toggle):
 	$LeftPortrait.visible = !toggle
@@ -49,20 +62,20 @@ func toggle_choice_mode(toggle):
 	$MarginContainer/ColorRect/TextNode.visible = !toggle
 	$MarginContainer/ColorRect/NextButton.visible = !toggle
 
-func read_narration_line(entry):
+func read_narration_line():
 		$LeftPortrait.visible = false
 		$RightPortrait.visible = false
 		$MarginContainer/ColorRect/NameNode.visible = false
-		$MarginContainer/ColorRect/TextNode/TextLabel.text = entry['text']
+		$MarginContainer/ColorRect/TextNode/TextLabel.text = current_script_entry['text']
 
-func read_conversation_line(entry):
+func read_conversation_line():
 		$LeftPortrait.visible = true
 		$RightPortrait.visible = true
 		$MarginContainer/ColorRect/NameNode.visible = true
-		$MarginContainer/ColorRect/NameNode/NameLabel.text = entry['name']
-		$MarginContainer/ColorRect/TextNode/TextLabel.text = entry['text']
+		$MarginContainer/ColorRect/NameNode/NameLabel.text = current_script_entry['name']
+		$MarginContainer/ColorRect/TextNode/TextLabel.text = current_script_entry['text']
 		
-		var active_portrait = entry['active_portrait']
+		var active_portrait = current_script_entry['active_portrait']
 		if (active_portrait == 'left'):
 			$LeftPortrait.modulate.a = 1
 			$RightPortrait.modulate.a = 0.5
@@ -71,10 +84,26 @@ func read_conversation_line(entry):
 			$LeftPortrait.modulate.a = 0.5
 			$RightPortrait.modulate.a = 1
 
-func read_choice_line(entry):
-	var player = $MarginContainer/AnimationPlayer
-	player.play("open_box")
-	yield(player, "animation_finished")
-	$ChoiceContainer/FirstChoiceButton.texture_normal = load(entry.left_choice.resource)
-	$ChoiceContainer/SecondChoiceButton.texture_normal = load(entry.right_choice.resource)
+func read_choice_line():
+	play_open_box_animation(true)
+	yield(_anim_player, "animation_finished")
+	$ChoiceContainer/FirstChoiceButton.texture_normal = load(current_script_entry.left_choice.resource)
+	$ChoiceContainer/SecondChoiceButton.texture_normal = load(current_script_entry.right_choice.resource)
 	$ChoiceContainer.visible = true
+
+func play_open_box_animation(open):
+	if (open):
+		_anim_player.play("open_box")
+	else:
+		_anim_player.play_backwards("open_box")
+
+func _on_FirstChoiceButton_pressed():
+	assert (current_script_entry && current_script_entry.left_choice)
+	$ChoiceContainer.visible = false
+	if (current_script_entry.left_choice.next_choice_index):
+		next_script_index = current_script_entry.left_choice.next_choice_index
+		play_open_box_animation(false)
+		yield(_anim_player, "animation_finished")
+		toggle_choice_mode(false)
+		next_line()
+		
