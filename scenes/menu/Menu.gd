4033c710d3b470ad0b2d926726ce04153851dac9
nico <frossard.nicolas@gmail.com>
2023-04-16 12:40:40 +0200

    Add transitions for intro animation

--- scenes/menu/Menu.gd
+++ scenes/menu/Menu.gd
@@ -1,8 +1,12 @@
 extends Control
 
+onready var _anim_player := $StartGameTransitionRect/AnimationPlayer
+
 func _on_QuitGameButton_pressed():
 	get_tree().quit()
 
 
 func _on_StartGameButton_pressed():
+	_anim_player.play("Fade")
+	yield(_anim_player, "animation_finished")
 	get_tree().change_scene("res://scenes/intro/Intro.tscn")
