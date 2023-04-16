dd371eb132bdc813aa2810aa13d341a044c79f68
nico <frossard.nicolas@gmail.com>
2023-04-16 14:43:31 +0200

    Fix intro

--- scenes/intro/Intro.gd
+++ scenes/intro/Intro.gd
@@ -5,7 +5,5 @@ onready var _anim_player := $AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_anim_player.play("Intro_animation")
-	$IntroBackground.visible = true
-	$ColorRect.visible = true
 	yield(_anim_player, "animation_finished")
 	get_tree().change_scene("res://scenes/house/house.tscn")
