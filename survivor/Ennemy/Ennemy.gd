939d2063738bccea4df69e19626d565b02e26c20
nico <frossard.nicolas@gmail.com>
2023-04-29 22:24:40 +0200

    Add animations

--- survivor/Ennemy/Ennemy.gd
+++ survivor/Ennemy/Ennemy.gd
@@ -3,8 +3,19 @@ extends KinematicBody2D
 export var movement_speed = 20.0
 
 onready var player = get_tree().get_nodes_in_group("player")[0]
+onready var sprite = $Sprite
+onready var animation_player = $AnimationPlayer
+
+func _ready():
+	animation_player.play("walk")
 
 func _physics_process(_delta):
 	var direction = global_position.direction_to(player.global_position)
 	var velocity = direction * movement_speed
 	move_and_slide(velocity)
+
+	# Face left or right, depending on velocity
+	if (velocity.x > 0):
+		sprite.flip_h = true
+	elif (velocity.x < 0):
+		sprite.flip_h = false
--- survivor/Player/Player.gd
+++ survivor/Player/Player.gd
@@ -3,6 +3,9 @@ extends KinematicBody2D
 var speed = 80  # speed in pixels/sec
 var velocity = Vector2.ZERO
 
+onready var sprite = $Sprite
+onready var walkTimer = get_node("WalkTimer")
+
 func get_input():
 	velocity = Vector2.ZERO
 	if Input.is_action_pressed('right'):
@@ -13,9 +16,25 @@ func get_input():
 		velocity.y += 1
 	if Input.is_action_pressed('up'):
 		velocity.y -= 1
-	# Make sure diagonal movement isn't faster
+	# Make sure diagonal movement isn't faster: use normalized
 	velocity = velocity.normalized() * speed
 
 func _physics_process(delta):
 	get_input()
+	
+	# Face left or right, depending on velocity
+	if (velocity.x > 0):
+		sprite.flip_h = true
+	elif (velocity.x < 0):
+		sprite.flip_h = false
+		
+	if (velocity != Vector2.ZERO):
+		if walkTimer.is_stopped():
+			if sprite.frame >= sprite.hframes - 1:
+				sprite.frame = 0
+			else:
+				sprite.frame = 1
+			walkTimer.start()
+	
 	velocity = move_and_slide(velocity)
+	
